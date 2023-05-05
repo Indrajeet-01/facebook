8292a936dc7e074ffff2273c7fde79fc56d848dc
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-05-05 19:00:53 +0530

    ceff4

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,6 +3,8 @@ const { generateToken } = require("../helpers/token")
 const { validateEmail, validateLength, validateUsername } = require("../helpers/validation")
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
 exports.register = async (req,res) => {
     try {
     const {
@@ -86,5 +88,48 @@ exports.register = async (req,res) => {
     })
     } catch(error) {
         res.status(500).json({message:error.message})
+    }
+}
+
+exports.activateAccount = async (req,res) => {
+    try{
+    const {token} = req.body
+    const user = jwt.verify(token,process.env.TOKEN_SECRET)
+    const check = await User.findById(user.id)
+    if (check.verified == true) {
+        return res.status(400).json({message: "this email is already exist"})
+    } else {
+        await User.findByIdAndUpdate(user.id, {verified: true})
+        return res.status(200).json({message: "Account has been activated"})
+    }} catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+exports.login = async (req,res) => {
+    try{
+        const {email,password} = req.body
+        const user = await User.findOne({email})
+        if (!user) {
+            return res.status(400).json({message: "This email is not registered"})
+        }
+        const check = await bcrypt.compare(password, user.password)
+        if (!check) {
+            return res.status(400).json({message: "Invalid credentials"})
+        }
+        const token = generateToken({ id: user._id.toString()},"7d")
+
+        res.send({
+            id:user._id,
+            username: user.username,
+            picture: user.picture,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            token: token,
+            verified: user.verified,
+            message: "login Success "
+        })
+    }catch(error){
+        res.status(500).json({message: error.message})
     }
 }
