ce64598e3073bcfa2018e07b05c9ff099dd9a87c
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-05-11 10:59:35 +0530

    stories

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -72,7 +72,10 @@ export default function Header() {
               setShowAllMenu((prev) => !prev);
             }}
           >
-                    <Menu/>
+            <div style={{transform:"translateY(2px"}}>
+            <Menu/>
+            </div>
+                    
                     </div>
                     {showAllMenu && <AllMenu/>}
                 </div>
@@ -91,7 +94,10 @@ export default function Header() {
               setShowUserMenu((prev) => !prev);
             }}
           >
-                    <ArrowDown/>
+            <div style={{transform:"translateY(2px"}}>
+            <ArrowDown/>
+            </div>
+                    
                     </div>
                     {showUserMenu && <UserMenu user={user}/> }
                 </div>
--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,16 +1,24 @@
 
 import { useSelector } from "react-redux";
 import Header from "../../components/header";
-import LeftLink from "../../components/home/left";
-import LeftHome from "../../components/home/left";
 
+import LeftHome from "../../components/home/left";
+import RightHome from "../../components/home/right";
+import Stories from "../../components/home/stories";
+import "./style.css"
+import CreatePost from "../../components/createPost";
 
 export default function Home() {
   const {user} = useSelector((user) => ({...user}))
   return (
-    <div>
+    <div className="home">
       <Header/>
       <LeftHome user={user}/>
+      <div className="home_middle">
+        <Stories/>
+        <CreatePost user={user} />
+      </div>
+      <RightHome user={user}/>
     </div>
     )
 }
