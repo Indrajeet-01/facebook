70f13bc8d669b55ae09a146671d0bd9b93f6d38b
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-05-15 00:05:23 +0530

    rfrf

--- backend/controllers/upload.js
+++ backend/controllers/upload.js
@@ -7,42 +7,9 @@ cloudinary.config({
 });
 exports.uploadImages = async (req, res) => {
   try {
-    const { path } = req.body;
-    let files = Object.values(req.files).flat();
-    let images = [];
-    for (const file of files) {
-      const url = await uploadToCloudinary(file, path);
-      images.push(url);
-      removeTmp(file.tempFilePath);
-    }
-    res.json(images);
+    res.json("welcome from image upload");
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 };
 
-const uploadToCloudinary = async (file, path) => {
-  return new Promise((resolve) => {
-    cloudinary.v2.uploader.upload(
-      file.tempFilePath,
-      {
-        folder: path,
-      },
-      (err, res) => {
-        if (err) {
-          removeTmp(file.tempFilePath);
-          return res.status(400).json({ message: "Upload image failed." });
-        }
-        resolve({
-          url: res.secure_url,
-        });
-      }
-    );
-  });
-};
-
-const removeTmp = (path) => {
-  fs.unlink(path, (err) => {
-    if (err) throw err;
-  });
-};
--- backend/middlewares/imageUpload.js
+++ backend/middlewares/imageUpload.js
@@ -19,7 +19,7 @@ module.exports = async function (req, res, next) {
         removeTmp(file.tempFilePath);
         return res.status(400).json({ message: "File size is too large." });
       }
-    });
+    });   
     next();
   } catch (error) {
     return res.status(500).json({ message: error.message });
